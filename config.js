bd464295ceacf96ba84e80f1a2b9027bb479caa6
hero5907-rgb <hero5907@gmail.com>
2026-05-29 15:12:29 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+// config.js (클럽별로 이 파일만 수정하면 됨)
+window.APP_CONFIG = {
+  apiUrl: "https://script.google.com/macros/s/AKfycbx8zdpGEpFyooxdf-9_fP6r5-9ApfZfKqAFPF3XE6qFTE2zZGBQeruoYBTVrcHxp_tG/exec",
+
+  clubName: "초 금 회",
+  district: "첫 금요일, 함께하는 병원행정관리자모임",
+
+  address: "경북 포항시",
+
+  phone: "010-7788-8883",
+
+  copyright: "Developed by Hwang Moongi, Class of 55, Handong",
+
+  // (선택) 기본 이미지 (설정시트에 로고/배경이 있으면 그게 우선)
+  logoUrl: "./logo.png",
+  coverUrl: "", // 예: "./cover.jpg" 또는 빈칸
+
+  // 🔥 여기 추가
+  introText: `
+첫 금요일, 함께하는 병원행정관리자모임
+`
+
+};
